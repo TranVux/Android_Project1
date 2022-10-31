e8fe7289f955c24f4418ed456298bb255ffbc15b
TranVux <vutran789jjjj@gmail.com>
2022-10-31 19:27:43 +0700

    init commit

---
+++ app/src/main/java/com/example/assignment_pro1121_nhom3/views/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.assignment_pro1121_nhom3.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.assignment_pro1121_nhom3.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
