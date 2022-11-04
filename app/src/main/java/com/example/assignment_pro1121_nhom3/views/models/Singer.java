60a755b45b79b935af0c01c057ca006d69a06b33
trieunnh5903 <111626907+trieunnh5903@users.noreply.github.com>
2022-11-04 16:04:32 +0700

    fininshed model

---
+++ app/src/main/java/com/example/assignment_pro1121_nhom3/views/models/Singer.java
@@ -0,0 +1,47 @@
+package com.example.assignment_pro1121_nhom3.views.models;
+
+public class Singer {
+    private String id;
+    private String name;
+    private String avtUrl;
+    private String desc;
+
+    public Singer(String id, String name, String avtUrl, String desc) {
+        this.id = id;
+        this.name = name;
+        this.avtUrl = avtUrl;
+        this.desc = desc;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAvtUrl() {
+        return avtUrl;
+    }
+
+    public void setAvtUrl(String avtUrl) {
+        this.avtUrl = avtUrl;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+}
