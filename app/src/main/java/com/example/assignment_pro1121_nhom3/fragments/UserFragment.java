885684f1b0b9845d5cf7b7afabc10f1ce417a3a9
tuanbasketball-2003 <nguyenductuanff2003@gmail.com>
2022-11-11 07:26:34 +0700

    màn hình cá nhân (3 layout chồng lên nhau )

--- app/src/main/java/com/example/assignment_pro1121_nhom3/fragments/UserFragment.java
+++ app/src/main/java/com/example/assignment_pro1121_nhom3/fragments/UserFragment.java
@@ -2,6 +2,8 @@ package com.example.assignment_pro1121_nhom3.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -15,12 +17,17 @@ public class UserFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_user, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
     }
 }
