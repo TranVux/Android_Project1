14ba58044ddcbb83a3c746e88b54d41f5c31010c
TranVux <vutran789jjjj@gmail.com>
2022-11-17 23:09:29 +0700

    Làm ở đây nè ba

---
+++ app/src/main/java/com/example/assignment_pro1121_nhom3/views/PlaylistActivity.java
@@ -0,0 +1,16 @@
+package com.example.assignment_pro1121_nhom3.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.assignment_pro1121_nhom3.R;
+
+public class PlaylistActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_playlist);
+    }
+}
