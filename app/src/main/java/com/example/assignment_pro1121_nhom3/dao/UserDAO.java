3098680963b810da0777f288a657cb8903f86d64
trieunnh5903 <111626907+trieunnh5903@users.noreply.github.com>
2022-11-24 10:00:54 +0700

    login

--- app/src/main/java/com/example/assignment_pro1121_nhom3/dao/UserDAO.java
+++ app/src/main/java/com/example/assignment_pro1121_nhom3/dao/UserDAO.java
@@ -8,6 +8,8 @@ import com.example.assignment_pro1121_nhom3.interfaces.IOnProgressBarStatusListe
 import com.example.assignment_pro1121_nhom3.models.Genres;
 import com.example.assignment_pro1121_nhom3.models.User;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
@@ -18,8 +20,34 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class UserDAO {
-//    private static final String TAG = UserDAO.class.getName();
-//    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private static final String TAG = UserDAO.class.getName();
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    public void addUserToFirestore(User user){
+        if(user != null){
+            Map<String, Object> data = new HashMap<>();
+            data.put("name", user.getName());
+            data.put("creationDate", user.getCreationDate());
+            data.put("email", user.getEmail());
+            data.put("isDelete", user.isDelete());
+            data.put("token", user.getToken());
+
+            db.collection("users").document(user.getId())
+                    .set(data)
+                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+                            Log.d(TAG, "DocumentSnapshot successfully written!");
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.w(TAG, "Error writing document", e);
+                        }
+                    });
+        }
+    }
 //
 //    public void getUser(IOnProgressBarStatusListener iOnProgressBarStatusListener, String id, ReadItemUser readItemUser){
 //        iOnProgressBarStatusListener.beforeGetData();
